0cfbbe8bba33b9b34d8d7a9cd695a79e88219fb0
eveocampos <eve.campos@wsu.edu>
2025-10-30 21:10:35 -0700

    updated click

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -3,7 +3,8 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     // responds to clicks on the burger icon
-    document.querySelector("navBurger").addEventListener("clicked", function(e){
+    document.querySelector("navBurger").addEventListener("click", function(e){
+        document.querySelector("nav").classList.toggle("clicked");
 
     });
 });
