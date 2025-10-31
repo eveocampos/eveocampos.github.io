fbe529e9e4dd1b4d60bc1b493bbd17c4157a6a3e
eveocampos <eve.campos@wsu.edu>
2025-10-30 21:02:06 -0700

    fixed JS

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,9 +1,9 @@
 // JS for responsive recipe
 
-document.addEventListener("DOMContentLoaded" ,function(){
+document.addEventListener("DOMContentLoaded", function(){
 
-    //respond to clicks on the burger icon
-    document.querySelector(#navBurger).addEventListener("click", function(e){
-        document.querySelector("nav").classList.toggle("clicked");
+    // responds to clicks on the burger icon
+    document.querySelector("navBurger").addEventListener("clicked", function(e){
+
     });
 });
