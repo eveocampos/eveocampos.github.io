137c089557bb05349a16c7ea719bd43116d0c02e
eveocampos <eve.campos@wsu.edu>
2025-12-10 22:29:05 -0800

    corrected nav

--- responsive-recipe/scripts.js
+++ responsive-recipe/scripts.js
@@ -1,18 +1,3 @@
-// set up DOM loading event listener 
-document.addEventListener("DOMContentLoaded", function () {
-
-    // find the dark mode button
-    let darkModeButton = document.querySelector("#toggleDarkMode");
-
-    // add a click function and handler
-    darkModeButton.addEventListener("click", function (e) {
-        
-        // when clicked, add class to the body called "darkMode"
-        document.body.classList.toggle("darkMode");
-        
-    });
-
-});
 // JS for responsive menu demo
 // file renamed to "scripts.js" (plural) after the demo
 
